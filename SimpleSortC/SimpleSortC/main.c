4ce1a59415ea8057107957dd550095f3f5253a7f
lindj0307 <lindongjie@lindongjiedeiMac.local>
2015-07-03 11:16:14 +0800

    Add SimpleSort

---
+++ SimpleSortC/SimpleSortC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SimpleSortC
+//
+//  Created by 林东杰 on 15/7/3.
+//  Copyright (c) 2015年 Anta. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
