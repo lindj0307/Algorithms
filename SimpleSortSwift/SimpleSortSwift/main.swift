be1eb0b85eeff0f23caa6151a57637c800738bbe
lindj0307 <lindongjie@lindongjiedeiMac.local>
2015-07-03 15:36:55 +0800

    add simple sort swift

---
+++ SimpleSortSwift/SimpleSortSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleSortSwift
+//
+//  Created by 林东杰 on 15/7/3.
+//  Copyright (c) 2015年 Anta. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
