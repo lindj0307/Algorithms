5116df3f8284cc9cf07ca90a00e465d77f938702
lindj0307 <lindongjie@lindongjiedeiMac.local>
2015-07-03 15:35:01 +0800

    Finish simpleSort

--- SimpleSortC/SimpleSortC/main.c
+++ SimpleSortC/SimpleSortC/main.c
@@ -9,7 +9,24 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int a[1001],i,j,t,n;
+    for (i=0; i<=1000; i++) {
+        a[i] =0;
+    }
+    printf("Please enter number Count:");
+    scanf("%d",&n);
+    for (i=1; i<=n; i++) {
+        printf("Please enter number %d:",i);
+        scanf("%d",&t);
+        a[t]++;
+    }
+    
+    printf("The result is :");
+    for (i=1000; i>=0; i--) {
+        for (j=1; j<=a[i]; j++) {
+            printf("%d ",i);
+        }
+    }
+    getchar();
     return 0;
 }
